2bc765e042bfd4fbab12004454d7d2a0dbd3f25c
robert-juru <juru0188@gmail.com>
2023-09-30 02:12:53 +0300

    Added task deletion functionality

--- src/project.js
+++ src/project.js
@@ -1,82 +1,96 @@
-import uiModule from './ui.js'
-import taskModule from './task.js';
+import uiModule from "./ui.js";
+import taskModule from "./task.js";
 
 const projectModule = (function () {
     //References to buttons and containers
-    const newProjectContainer = document.querySelector('.new-project-container');
-    const projectsContainer = document.querySelector('.projects-container');
-    const projectSectionContainer = document.querySelector('.projects-section-container');
-    const projectSelector = document.getElementById('project-selector');
-    const newProjectInput = document.getElementById('new-project');
-    const mainSection = document.querySelector('main');
-    const alertMessage = createAlertMessage('Project name is not valid. Please choose a different name.');
-    let projectList = []
+    const newProjectContainer = document.querySelector(".new-project-container");
+    const projectsContainer = document.querySelector(".projects-container");
+    const projectSectionContainer = document.querySelector(
+        ".projects-section-container"
+    );
+    const projectSelector = document.getElementById("project-selector");
+    const newProjectInput = document.getElementById("new-project");
+    const mainSection = document.querySelector("main");
+    const alertMessage = createAlertMessage(
+        "Project name is not valid. Please choose a different name."
+    );
+    let projectList = [];
     const createdProjectNames = [];
 
     // Create and return an ion-icon element
     function createProjectIcon() {
-        let projectIcon = document.createElement('ion-icon')
-        projectIcon.setAttribute('name', 'menu-outline')
-        projectIcon.setAttribute('size', 'small')
-        projectIcon.setAttribute('id', 'menu-project-icon');
+        let projectIcon = document.createElement("ion-icon");
+        projectIcon.setAttribute("name", "menu-outline");
+        projectIcon.setAttribute("size", "small");
+        projectIcon.setAttribute("id", "menu-project-icon");
         return projectIcon;
     }
 
     function createDeleteProjectIcon() {
-        let deleteProjectIcon = document.createElement('ion-icon')
-        deleteProjectIcon.setAttribute('name', 'trash-outline')
-        deleteProjectIcon.setAttribute('size', 'small')
-        deleteProjectIcon.setAttribute('id', 'delete-project-btn');
+        let deleteProjectIcon = document.createElement("ion-icon");
+        deleteProjectIcon.setAttribute("name", "trash-outline");
+        deleteProjectIcon.setAttribute("size", "small");
+        deleteProjectIcon.setAttribute("id", "delete-project-btn");
 
         return deleteProjectIcon;
     }
 
-
     // Create a project element and return it
     function createProjectElement(projectName) {
-        const projectElement = document.createElement('div');
+        const projectElement = document.createElement("div");
 
-        const project = document.createElement('div');
-        project.setAttribute('id', 'project-name')
+        const project = document.createElement("div");
+        project.setAttribute("id", "project-name");
         project.textContent = projectName;
 
         projectElement.appendChild(createProjectIcon());
-        projectElement.appendChild(project)
+        projectElement.appendChild(project);
         projectElement.appendChild(createDeleteProjectIcon());
 
-        projectElement.classList.add('nav-btn', 'project');
-        projectElement.setAttribute('id', `${projectName}`);
-        projectList.push(projectElement)
+        projectElement.classList.add("nav-btn", "project");
+        projectElement.setAttribute("id", `${projectName}`);
+        projectList.push(projectElement);
         return projectElement;
     }
 
     function deleteProject(event) {
-        const deleteIcon = event.target;
-        const projectElementToDelete = deleteIcon.closest('.project');
+        const deleteProjectIcon = event.target;
+        const projectElementToDelete = deleteProjectIcon.closest(".project");
         if (projectElementToDelete) {
-            const projectName = projectElementToDelete.getAttribute('id');
+            const projectName = projectElementToDelete.getAttribute("id");
             // Remove the project element from the DOM
             projectElementToDelete.remove();
             // Remove the project from the projectList & the project name from the createdProjectNames
-            const projectIndex = projectList.findIndex(project => project.getAttribute('id') === projectName);
+            const projectIndex = projectList.findIndex(
+                (project) => project.getAttribute("id") === projectName
+            );
             if (projectIndex !== -1) {
                 projectList.splice(projectIndex, 1);
                 createdProjectNames.splice(projectIndex, 1);
             }
-            console.log(projectList)
             // Remove the project page container
-            const projectPageContainer = document.getElementById(`${projectName}-page-container`)
-            mainSection.removeChild(projectPageContainer)
+            const projectPageContainer = document.getElementById(`${projectName}-page-container`);
+            if (projectPageContainer) {
+                mainSection.removeChild(projectPageContainer);
+            }
+            // mainSection.removeChild(projectPageContainer);
             // Toggle page container to All Tasks
-            uiModule.toggleTaskSection('allTasks')
+            uiModule.toggleTaskSection("allTasks");
         }
     }
 
+    // Attach the deleteProject function to your delete project icons
+    document.addEventListener("click", function (event) {
+        if (event.target && event.target.id === "delete-project-btn") {
+            deleteProject(event);
+        }
+    });
+
     function createProjectPageContainer(projectName) {
-        const projectPageContainer = document.createElement('div');
-        projectPageContainer.classList.add('project-page-container');
-        projectPageContainer.setAttribute('id', `${projectName}-page-container`); // Use the project name as the container's ID
-        projectPageContainer.style.display = 'none'; // Initially hide the container
+        const projectPageContainer = document.createElement("div");
+        projectPageContainer.classList.add("project-page-container");
+        projectPageContainer.setAttribute("id", `${projectName}-page-container`); // Use the project name as the container's ID
+        projectPageContainer.style.display = "none"; // Initially hide the container
         mainSection.appendChild(projectPageContainer);
         return projectPageContainer;
     }
@@ -87,8 +101,10 @@ const projectModule = (function () {
     }
 
     function showProjectContainer(projectName) {
-        const selectedContainer = document.getElementById(`${projectName}-page-container`);
-        selectedContainer.style.display = 'block';
+        const selectedContainer = document.getElementById(
+            `${projectName}-page-container`
+        );
+        selectedContainer.style.display = "block";
     }
 
     // Add a new project to the projects list
@@ -98,29 +114,30 @@ const projectModule = (function () {
         if (projectName && !createdProjectNames.includes(projectName)) {
             const project = createProjectElement(projectName);
             projectsContainer.appendChild(project);
-            newProjectInput.value = ''; // input reset
-            newProjectContainer.style.display = 'none';
+            newProjectInput.value = ""; // input reset
+            newProjectContainer.style.display = "none";
             // Append a new project container when a project is created
-            appendProjectContainer(projectName)
+            appendProjectContainer(projectName);
             // Select the appended project container
-            const selectedContainer = document.getElementById(`${projectName}-page-container`);
+            const selectedContainer = document.getElementById(
+                `${projectName}-page-container`
+            );
             // Append the header for the container
             uiModule.createMainHeader(projectName, selectedContainer);
             // Store the project name for duplication check
             createdProjectNames.push(projectName);
             // If the project name is unique and valid, remove any previous duplication message
             removeDuplicateProjectAlert(alertMessage);
-
         } else {
             // Handle the case where the project name already exists
-            alertDuplicateProject()
+            alertDuplicateProject();
         }
     }
 
     function createAlertMessage(textContent) {
-        const alertMessage = document.createElement('div');
+        const alertMessage = document.createElement("div");
         alertMessage.textContent = textContent;
-        alertMessage.style.paddingTop = '10px';
+        alertMessage.style.paddingTop = "10px";
         return alertMessage;
     }
 
@@ -135,45 +152,61 @@ const projectModule = (function () {
     }
     // Display the new project input container
     function showNewProjectContainer() {
-        newProjectContainer.style.display = 'block';
+        newProjectContainer.style.display = "block";
     }
     // Hide the new project input container and reset the input
     function hideNewProjectContainer() {
-        const newProjectInput = document.getElementById('new-project');
-        newProjectInput.value = ''; // input reset
-        newProjectContainer.style.display = 'none';
+        const newProjectInput = document.getElementById("new-project");
+        newProjectInput.value = ""; // input reset
+        newProjectContainer.style.display = "none";
         removeDuplicateProjectAlert(alertMessage);
     }
     // Add projects to the task modal project selector
     function populateProjectSelector() {
         // Clear only dynamically added options
-        while (projectSelector.lastChild && projectSelector.lastChild.value !== 'all-tasks') {
+        while (
+            projectSelector.lastChild &&
+            projectSelector.lastChild.value !== "all-tasks"
+        ) {
             projectSelector.removeChild(projectSelector.lastChild);
         }
-        const projectItems = projectsContainer.querySelectorAll('.nav-btn.project');
+        const projectItems = projectsContainer.querySelectorAll(".nav-btn.project");
         // Append the project items to the project selector
-        projectItems.forEach(projectItem => {
-            const option = document.createElement('option');
+        projectItems.forEach((projectItem) => {
+            const option = document.createElement("option");
             option.value = projectItem.textContent;
             option.textContent = option.value;
             projectSelector.appendChild(option);
-        })
+        });
     }
 
     function addTaskToProjectContainer(task) {
-        let card = taskModule.createTaskCard(task)
-        const projectContainer = document.getElementById(`${task.project}-page-container`);
+        let card = taskModule.createTaskCard(task);
+        const projectContainer = document.getElementById(
+            `${task.project}-page-container`
+        );
         //Append the task to the project container
-        const existingTaskCard = projectContainer.querySelector(`.task-card[data-task-id="${task.id}"]`);
-        if (!existingTaskCard) {
-            projectContainer.appendChild(card);
-        }
-        else {
-            projectContainer.removeChild(existingTaskCard);
-            projectContainer.appendChild(card);
+        if (projectContainer) {
+            const existingTaskCard = projectContainer.querySelector(
+                `.task-card[data-task-id="${task.id}"]`
+            );
+            if (!existingTaskCard) {
+                projectContainer.appendChild(card);
+            } else {
+                projectContainer.removeChild(existingTaskCard);
+                projectContainer.appendChild(card);
+            }
         }
     }
 
-    return { addNewProject, showNewProjectContainer, hideNewProjectContainer, populateProjectSelector, showProjectContainer, addTaskToProjectContainer, deleteProject }
-})()
+    return {
+        addNewProject,
+        showNewProjectContainer,
+        hideNewProjectContainer,
+        populateProjectSelector,
+        showProjectContainer,
+        addTaskToProjectContainer,
+        deleteProject
+    };
+})();
 export default projectModule;
--- src/task.js
+++ src/task.js
@@ -10,8 +10,10 @@ const taskModule = (function () {
     const mainSection = document.querySelector('main');
     const today = format(new Date(), "MMM d, yyyy");
     const taskList = [];
+    let copyOfTaskList = [];
     let taskIdCounter = 0;
     //Create a task object 
+
     function createTask(title, description, dueDate, priority, project) {
         const task = {
             id: taskIdCounter++,
@@ -29,14 +31,22 @@ const taskModule = (function () {
             section.removeChild(taskCard);
         });
     }
+
     function renderAllSections() {
-        // Clear the content of the all tasks container
+        // Clear the content of the sections
         clearTasksInSection(allTasksContainer);
+        clearTasksInSection(todayTasksContainer);
+        clearTasksInSection(importantTasksContainer);
+
         // Loop through the taskList and create task cards for each task
         taskList.forEach(task => {
-            updateTaskUI(task);
+            updateTaskUI(task, allTasksContainer); // Append to All Tasks
+            updateTaskUI(task, todayTasksContainer); // Append to Today Tasks
+            updateTaskUI(task, importantTasksContainer); // Append to Important Tasks
+            // }
         });
     }
+
 
     function filterTasksForToday() {
         const today = new Date();
@@ -46,6 +56,11 @@ const taskModule = (function () {
             taskDueDate.setHours(0, 0, 0, 0);
             return today.getTime() === taskDueDate.getTime();
         });
+    }
+
+    function displayTasksForToday() {
+        const todayTasks = filterTasksForToday();
+        updateTodayTasksUI(todayTasks);
     }
 
     function stylizeTaskCardByPriority(taskCard, priority) {
@@ -75,28 +90,11 @@ const taskModule = (function () {
     <div class="task-actions">
         <p id="task-due-date" class="editable-date">${formattedDueDate}</p>
         <ion-icon name="create-outline"></ion-icon>
-        <ion-icon name="trash-outline"></ion-icon>
+        <ion-icon id="delete-task-btn" name="trash-outline"></ion-icon>
     </div>
         `;
         stylizeTaskCardByPriority(card, task.priority);
         return card;
-    }
-
-    function editTaskCard() {
-        mainSection.addEventListener('change', function (event) {
-            const target = event.target;
-            if (target.classList.contains('task-status-checkbox')) {
-                const taskName = target.closest('.task-card').querySelector('.task-name');
-                const taskCard = target.closest('.task-card');
-                if (target.checked) {
-                    taskName.style.textDecoration = 'line-through';
-                    taskCard.style.color = '#696969';
-                } else {
-                    taskName.style.textDecoration = 'none';
-                    taskCard.style.color = 'black';
-                }
-            }
-        });
     }
 
     function extractTaskFromForm() {
@@ -108,11 +106,6 @@ const taskModule = (function () {
         const project = taskForm.querySelector('#project-selector').value;
 
         return createTask(title, description, dueDate, priority, project);
-    }
-
-    function displayTasksForToday() {
-        const todayTasks = filterTasksForToday();
-        updateTodayTasksUI(todayTasks);
     }
 
     function updateTodayTasksUI(todayTasks) {
@@ -148,14 +141,14 @@ const taskModule = (function () {
         event.preventDefault();
         const task = extractTaskFromForm(); // Extract task data from the form 
         addTaskToList(task); // Add the task to the list
-        updateTaskUI(task) // Create the visual task card
+        updateTaskUI(task, allTasksContainer) // Create the visual task card
         taskForm.reset(); //Reset the form
         uiModule.toggleTaskModal(); // Close the modal
-        projectModule.addTaskToProjectContainer(task);
     }
-    function updateTaskUI(task) {
+
+    function updateTaskUI(task, container) {
         let card = createTaskCard(task); // Create the visual task card
-        allTasksContainer.appendChild(card); // Append the visual task card to All Tasks
+        container.appendChild(card);
         // Check if the new task's due date is today
         const formattedDueDate = format(new Date(task.dueDate), "MMM d, yyyy");
         if (formattedDueDate === today) {
@@ -174,6 +167,42 @@ const taskModule = (function () {
     function addTaskToList(task) {
         taskList.push(task);
     }
+
+    function deleteTask(event) {
+        const deleteTaskIcon = event.target;
+        const taskToDelete = deleteTaskIcon.closest('.task-card');
+        const taskCardId = taskToDelete.getAttribute('data-task-id');
+
+        // Find the task in the taskList
+        const taskIndex = taskList.findIndex(task => task.id.toString() === taskCardId);
+
+        if (taskToDelete && taskIndex !== -1) {
+            //Capture task information
+            const task = taskList[taskIndex];
+            // Remove the task from the DOM
+            taskToDelete.remove();
+            // Remove the task from the taskList
+            taskList.splice(taskIndex, 1);
+            // Remove the task from the project container
+            if (task.project !== 'all-tasks') {
+                const projectContainer = document.getElementById(task.project + '-page-container');
+                console.log(projectContainer)
+                if (projectContainer) {
+                    const taskInProject = projectContainer.querySelector(`[data-task-id="${taskCardId}"]`);
+                    if (taskInProject) {
+                        taskInProject.remove();
+                    }
+                }
+            }
+            console.log(taskList);
+        }
+    }
+
+    document.addEventListener("click", function (event) {
+        if (event.target && event.target.id == "delete-task-btn")
+            deleteTask(event)
+    })
+
     return { createTask, createTaskCard, handleFormSubmit, displayTasksForToday, displayImportantTasks, taskList, renderAllSections }
 })()
 export default taskModule;
